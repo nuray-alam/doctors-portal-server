92c7e4dd4e3677d3e87cef83bc644916ae74072e
nuray-alam <siddik4x@gmail.com>
2021-11-09 13:57:32 +0600

    all routes added

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+var cors = require('cors')
+const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
+
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+//connecting to the database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1yqsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+
+    try {
+        await client.connect();
+        const database = client.db('doctors_portal')
+        const appointmentsCollection = database.collection('appointments');
+        const usersCollection = database.collection('users');
+
+        app.get('/appointments', async (req, res) => {
+            const email = req.query.email;
+            const date = new Date(req.query.date).toLocaleDateString();
+            const query = { email: email, date: date }
+            const cursor = appointmentsCollection.find(query)
+            const appointments = await cursor.toArray();
+            res.json(appointments);
+
+        });
+
+
+        app.get('/users/:email', async (req, res) => {
+
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+
+        })
+
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            console.log(result);
+            res.json(result);
+
+        })
+
+
+        app.post('/users', async (req, res) => {
+
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            // create a document that sets the plot of the movie
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+
+    }
+
+    finally {
+
+        // await client.close();
+    }
+
+}
+
+
+run().catch(console.dir)
+
+
+app.get('/', (req, res) => {
+    res.send('Hello doctors portal')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
